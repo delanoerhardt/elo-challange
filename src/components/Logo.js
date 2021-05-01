31bc1d0ff08a51f422c8e42c4b98eaaaaad041e4
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-05-01 13:00:27 -0300

    Added ability to control max-width of logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,10 @@
 import "./Logo.css";
 import logoPng from "../logo-EloGroup-branco.png";
 
-function Logo() {
+function Logo(props) {
+  const inlineStyle = { maxWidth: props.maxSize };
   return (
-    <div className="logo">
+    <div className="logo" style={inlineStyle}>
       <img className="logo-png" src={logoPng} alt="EloGroup logo" />
     </div>
   );
