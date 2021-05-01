612dc8f87810e7fc35ac89e2f775580fcbee3ec4
Gabriel Delolmo <delanoerhardt@gmail.com>
2021-05-01 16:27:27 -0300

    Refactor of the register form

--- src/routes/RegisterPage.js
+++ src/routes/RegisterPage.js
@@ -1,13 +1,66 @@
+import "./RegisterPage.css";
+
+import { useState } from "react";
 import Outline from "../components/Outline";
 import Logo from "../components/Logo";
-import RegisterForm from "../components/RegisterForm";
+import FormSection from "../components/FormSection";
+import CustomButton from "../components/CustomButton";
 
 function RegisterPage() {
+  const [formState, setFormState] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (event) => {
+    event.preventDefault();
+
+    const newFormState = { ...formState };
+    newFormState[event.target.id] = event.target.value;
+
+    setFormState(newFormState);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(formState);
+  };
+
   return (
     <div className="center">
       <Outline>
         <Logo />
-        <RegisterForm />
+        <form className="register-form" onSubmit={handleSubmit}>
+          <FormSection
+            id="username"
+            type="text"
+            valueOfState={formState.username}
+            handleChange={handleChange}
+          >
+            Usuário *
+          </FormSection>
+          <FormSection
+            id="password"
+            type="password"
+            valueOfState={formState.password}
+            handleChange={handleChange}
+          >
+            Senha *
+          </FormSection>
+          <FormSection
+            id="confirmPassword"
+            type="password"
+            valueOfState={formState.confirmPassword}
+            handleChange={handleChange}
+          >
+            Confirmação da senha *
+          </FormSection>
+          <div className="form-section">
+            <CustomButton type="submit" color="white" value="Registrar" />
+          </div>
+        </form>
       </Outline>
     </div>
   );
